aa4f1d2a11a2eec9243fca87828db743b075fc5b
Jesse Ziros <jtziros2020@gmail.com>
2020-10-11 23:04:11 -0400

    created helper file to assist with functionality of created notes

---
+++ db/helper.js
@@ -0,0 +1,15 @@
+class helper {
+    read() {
+        //return contents of db.json file (read the file)
+      }
+    
+      write() {
+        //write to the db.json file (write the file)
+      }
+    
+      allNotes() {
+        //use read()
+       //parse the contents to an array or return an empty array
+      }
+        
+}
