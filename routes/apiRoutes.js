3416bcd328d780d424d552e0130fb7602cc77df5
Jesse Ziros <jtziros2020@gmail.com>
2020-10-13 21:07:12 -0400

    removed helper.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,18 +4,21 @@ const db = require('../db/db.json')
 
 router.get('/notes', (req, res) => {
     res.send(db)
+    console.log(db)
 });
 
 router.post('/notes', (req, res) => {
     db.push(req.body)
+    console.log(req.body)
     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
         if (err) throw err 
         res.send('Completed')
     })
 });
 
-// router.delete('/notes/:id'), (req, res) => {
-
-// }
+router.delete('/notes/:id'), (req, res) => {
+    res.send(db)
+    fs.writeFile('./db/db.json', JSON.stringify(db))
+}
 
 module.exports = router
