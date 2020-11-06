ffa11c77b10fadf96eb40b0924c3ddcae0ad4035
jesseziros <jtziros2020@gmail.com>
2020-11-05 21:05:53 -0500

    Completed product, notes add and delete as needed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const router = require('express').Router();
 const db = require('../db/db.json')
+const uuid = require('uuid/v1')
 
 router.get('/notes', (req, res) => {
     res.send(db)
@@ -8,6 +9,7 @@ router.get('/notes', (req, res) => {
 });
 
 router.post('/notes', (req, res) => {
+    req.body.id = uuid();
     db.push(req.body)
     console.log(req.body)
     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
@@ -16,9 +18,17 @@ router.post('/notes', (req, res) => {
     })
 });
 
-router.delete('/notes/:id'), (req, res) => {
-    res.send(db)
-    fs.writeFile('./db/db.json', JSON.stringify(db))
-}
+router.delete('/notes/:id', (req, res) => {
+    console.log("Hey")
+    for (let i = 0; i < db.length; i++) {
+        if (req.params.id == db[i].id) {
+            db.splice(i, 1)
+        }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        if (err) throw err;
+        res.send(db)
+    })
+})
 
 module.exports = router
